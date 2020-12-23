439a595145dc2fa143da90667e0f19c5e6ca3d52
Nikola Spiric <nikola.spiric@rt-rk.com>
2020-12-23 21:43:38 +0100

    Fix permission checking

--- app/src/main/java/com/limber/breach/MainActivity.java
+++ app/src/main/java/com/limber/breach/MainActivity.java
@@ -4,25 +4,22 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import com.google.android.material.snackbar.Snackbar;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String kTAG = MainActivity.class.getSimpleName();
 
-    private static final int PERMISSION_ALL = 1;
-    private boolean mPermissionsChecked = false;
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+
     private static final String[] PERMISSIONS = {
-            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
             android.Manifest.permission.CAMERA
     };
 
@@ -30,10 +27,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-
-        if (!mPermissionsChecked) {
+        if (!allPermissionsGranted()) {
             checkPermissions();
+        } else {
+            start();
+        }
+    }
+
+    void start() {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Snackbar.make(getWindow().getDecorView().getRootView(),
+                        "Permissions not granted by user..", Snackbar.LENGTH_LONG)
+                        .show();
+                finish();
+            } else {
+                start();
+            }
         }
     }
 
@@ -41,11 +58,21 @@ public class MainActivity extends AppCompatActivity {
         if (!hasPermissions(this, PERMISSIONS)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(PERMISSIONS,
-                        PERMISSION_ALL);
+                        REQUEST_CODE_PERMISSIONS);
             }
-            mPermissionsChecked = false;
         }
-        mPermissionsChecked = true;
+
+    }
+
+    boolean allPermissionsGranted() {
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+
+        return true;
+
 
     }
 
